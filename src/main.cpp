e46a481482f7ead2d6e98b3beb78322baeca433b
Matiss Abolins <matiss.ab1@gmail.com>
2022-12-27 18:29:49 +0100

    cool reflective sphere made out of voxels

--- src/main.cpp
+++ src/main.cpp
@@ -29,21 +29,26 @@ int main() {
   PixelBuffer pixelbuffer(&config);
   Camera camera(&config);
 
-  Material glass = new_material_refractive(RGBi("purple"), 0.3, 1.5);
+  Material glass = new_material_refractive(RGBi("purple"), 0.2, 1.5);
   Material mirror = new_material_reflective(RGBi("white"), 0.2);
-  Material orange = new_material_standard(RGBi("orange"), 0.3, 0.8);
+  Material orange = new_material_standard(RGBi("green"), 0.3, 0.8);
   Octree root(&config);
-  srand(0);
-  root.fill("sphere", 3, &glass, false);
-  srand(1);
-  root.fill("sphere", 3, &mirror, false);
-  srand(2);
-  root.fill("sphere", 20, &orange, false);
+  /* srand(0); */
+  /* root.fill("sphere", 3, &glass, false); */
+  /* srand(10); */
+  /* root.fill("sphere", 3, &mirror, false); */
+  /* srand(2); */
+  /* root.fill("sphere", 20, &orange, false); */
+  root.fill("sphere", 50000, &mirror, false);
+  std::cout << "Voxels inserted: " << root.count_voxels() << std::endl;
 
   Renderer renderer(&config, &pixelbuffer, &camera, &root);
   Clock clock(&config);
   Terminal terminal(&config);
   show_cursor(false);
+
+  std::cout << "Press Enter to start\n";
+  std::cin.ignore();
 
   /* main loop */
   float cam_angle = 0; // rad
